75600df4bb8af8785a470a67171d1d87d5c8af75
Jean-Poul <cph-jl360@cphbusiness.dk>
2022-05-05 14:40:51 +0200

    alldata to database

--- server/routes/redis.js
+++ server/routes/redis.js
@@ -1,6 +1,7 @@
 const redis = require("redis");
 // console.log(redis);
 const client = redis.createClient();
+const axios = require("axios");
 
 // This is connectiong to on default port on lcalhost, to connect to a specific server provide data like this:
 // createClient({
@@ -8,6 +9,8 @@ const client = redis.createClient();
 // });
 
 const express = require("express");
+const req = require("express/lib/request");
+const { response } = require("express");
 
 const redisRouts = express.Router();
 
@@ -17,12 +20,44 @@ redisRouts.route("/redis").get(function (req, res) {
 
 redisRouts.route("/add_value").post(async (req, res) => {
   await client.connect();
-
+  console.log(req.body);
   const key = Object.getOwnPropertyNames(req.body)[0];
   const value = req.body[key];
   await client.set(key, value);
 
   const added = await client.get("mykey");
   res.json({ added });
+});
+
+redisRouts.route("/all").get(async (req, res) => {
+  const url =
+    "https://datausa.io/api/data?measures=Average%20Wage,Average%20Wage%20Appx%20MOE&drilldowns=Detailed%20Occupation";
+  await client.connect();
+
+  let added = await client.get("alldata");
+
+  // res.json({ added })
+  console.log("redis result" + added);
+  if (added === null) {
+    await axios.get(url).then(
+      (response) => {
+        //console.log(JSON.stringify(response.data));
+        client.set("alldata", JSON.stringify(response.data));
+        added = response.data;
+        console.log(typeof response.data);
+        client.quit();
+      },
+      (error) => {
+        console.error("Error:", error);
+      }
+    );
+  } else {
+    added = JSON.parse(added);
+    console.log("else:" + added);
+  }
+
+  res.json({ added });
+
+  await client.quit();
 });
 module.exports = redisRouts;
